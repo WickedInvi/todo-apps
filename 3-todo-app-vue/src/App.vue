81d1f0dbe94eea79553810c45cc12b84b18b6740
Deyan Petrov <deyanpetrov89@yahoo.com>
2022-06-30 21:47:14 +0200

    Vue initial

---
+++ 3-todo-app-vue/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+import InputForm from './components/InputForm.vue';
+
+let todoArr = [
+  { task: 'Learn Vue', done: false },
+  { task: 'Learn Angular', done: false },
+];
+</script>
+
+<template>
+  <InputForm :todoArr="todoArr" />
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: top;
+  }
+  /* 
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  } */
+}
+</style>
---
+++ 3-todo-app-vue/src/components/InputForm.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  todoArr: {
+    type: [Array, Object],
+    required: true,
+  },
+});
+
+const submitHandler = (e) => {
+  // e.preventDefault();
+  // props.todoArr.forEach((todo) => {
+  //   console.log(todo.task);
+  // });
+  // console.log(props.todoArr);
+  console.log(taskInput.value);
+};
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="">
+      <input
+        type="text"
+        name="taskInput"
+        id="taskInput"
+        placeholder="Add a task here"
+        value="test"
+      />
+      <button>Add task</button>
+    </form>
+    <ul>
+      <li
+        @click="submitHandler"
+        v-for="todo in todoArr"
+        :key="todo.task"
+      >
+        {{ todo.task }}
+      </li>
+    </ul>
+  </div>
+</template>
