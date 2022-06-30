91295a76722613fc36ab2ed756609b969cd6661e
Deyan Petrov <deyanpetrov89@yahoo.com>
2022-06-30 15:57:13 +0200

    feat: added save and done

--- 1-todo-app-vanilla/index.js
+++ 1-todo-app-vanilla/index.js
@@ -32,6 +32,9 @@ const createTodo = (arr) => {
     let deleteBtn = document.createElement('button');
     let editBtn = document.createElement('button');
     let editInput = document.createElement('input');
+    let checkBox = document.createElement('input');
+
+    checkBox.type = 'checkbox';
 
     // Assign the values and style
     task.innerHTML = todo.task;
@@ -46,6 +49,11 @@ const createTodo = (arr) => {
     deleteBtn.addEventListener('click', () => {
       deleteTodo(id);
     });
+
+    checkBox.addEventListener('change', () => {
+      console.log(id);
+      doneTask(id);
+    });
     editBtn.addEventListener('click', () => {
       editBtn.textContent =
         editBtn.textContent === 'Edit' ? 'Save' : 'Edit';
@@ -56,7 +64,7 @@ const createTodo = (arr) => {
     editInput.addEventListener('focus', () => {
       editInput.value = editInput.placeholder;
     });
-
+    newLi.appendChild(checkBox);
     newLi.appendChild(editInput);
     newLi.appendChild(task);
     newLi.appendChild(deleteBtn);
@@ -69,15 +77,27 @@ const editTodo = (id) => {
   let inputField = document.getElementById(`input-${id}`);
   let task = document.getElementById(`task-${id}`);
 
-  // console.log(inputField);
   if (inputField.classList.contains('hidden')) {
     inputField.classList.remove('hidden');
     task.classList.add('hidden');
     inputField.placeholder = task.innerText;
   } else {
+    console.log(inputField.value);
+    todoArr.splice(id, 1, { task: inputField.value });
+    console.log(todoArr);
+    createTodo(todoArr);
     task.classList.remove('hidden');
     inputField.classList.add('hidden');
   }
+};
+
+const doneTask = (id) => {
+  console.log('done');
+  let task = document.getElementById(`task-${id}`);
+  console.log(task);
+  task.classList.contains('strike')
+    ? task.classList.remove('strike')
+    : task.classList.add('strike');
 };
 
 const deleteTodo = (id) => {
