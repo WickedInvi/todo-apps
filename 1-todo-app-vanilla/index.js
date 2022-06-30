70ee7f450f97560ebec5903a8b1fad4bbb8cbc3c
Deyan Petrov <deyanpetrov89@yahoo.com>
2022-06-30 20:16:37 +0200

    Refactoring and bug fix

--- 1-todo-app-vanilla/index.js
+++ 1-todo-app-vanilla/index.js
@@ -2,14 +2,10 @@ const addTask = document.querySelector('#btn-addTask');
 const inputTask = document.querySelector('#text-todoTask');
 const todoList = document.querySelector('#todo-list');
 
-// console.log(inputTask.textContent);
-
 let todoArr = [{ task: 'Make coffee' }, { task: 'Drink coffee' }];
 
 addTask.addEventListener('click', (event) => {
   event.preventDefault();
-  // console.log(inputTask.value);
-
   if (inputTask.value === '') {
     alert('Task is empty, please add a task');
   } else {
@@ -21,39 +17,78 @@ addTask.addEventListener('click', (event) => {
   createTodo(todoArr);
 });
 
-inputTask.addEventListener('keyup', () => {});
-
 const createTodo = (arr) => {
   todoList.innerHTML = '';
   arr.forEach((todo, id) => {
     // Create elements required
     let newLi = document.createElement('li');
     let task = document.createElement('span');
+    let taskDiv = document.createElement('div');
     let deleteBtn = document.createElement('button');
     let editBtn = document.createElement('button');
     let editInput = document.createElement('input');
     let checkBox = document.createElement('input');
-
-    checkBox.type = 'checkbox';
+    let newDiv = document.createElement('div');
+    let yesBtn = document.createElement('button');
+    let noBtn = document.createElement('button');
 
     // Assign the values and style
+
+    // Div
+    newDiv.classList.add('hidden');
+    newDiv.id = `div-${id}`;
+
+    // Yes Button
+    yesBtn.innerText = 'Yes';
+
+    // No Button
+    noBtn.innerText = 'No';
+
+    // Check Box
+    checkBox.type = 'checkbox';
+
+    // Task
     task.innerHTML = todo.task;
-    task.id = `task-${id}`;
+    taskDiv.id = `task-${id}`;
+
+    // Input
     editInput.id = `input-${id}`;
+
+    // Delete Button
     deleteBtn.innerText = 'X';
     deleteBtn.style.color = 'red';
+
+    // Edit Button
     editBtn.innerText = 'Edit';
 
+    // Edit Input
     editInput.classList.add('hidden');
-    // Add ID to the button
+
     deleteBtn.addEventListener('click', () => {
+      let task = document.getElementById(`task-${id}`);
+      let yesNoDiv = document.getElementById(`div-${id}`);
+
+      task.classList.add('hidden');
+      yesNoDiv.classList.remove('hidden');
+    });
+
+    yesBtn.addEventListener('click', () => {
       deleteTodo(id);
     });
 
-    checkBox.addEventListener('change', () => {
+    noBtn.addEventListener('click', () => {
+      let task = document.getElementById(`task-${id}`);
+      let yesNoDiv = document.getElementById(`div-${id}`);
+
+      task.classList.remove('hidden');
+      yesNoDiv.classList.add('hidden');
+    });
+
+    checkBox.addEventListener('change', (event) => {
       console.log(id);
       doneTask(id);
     });
+
     editBtn.addEventListener('click', () => {
       editBtn.textContent =
         editBtn.textContent === 'Edit' ? 'Save' : 'Edit';
@@ -64,9 +99,17 @@ const createTodo = (arr) => {
     editInput.addEventListener('focus', () => {
       editInput.value = editInput.placeholder;
     });
-    newLi.appendChild(checkBox);
+
+    // Add the elements to the DOM
+    newDiv.appendChild(yesBtn);
+    newDiv.appendChild(noBtn);
+
+    taskDiv.appendChild(checkBox);
+    taskDiv.appendChild(task);
+
+    newLi.appendChild(taskDiv);
+    newLi.appendChild(newDiv);
     newLi.appendChild(editInput);
-    newLi.appendChild(task);
     newLi.appendChild(deleteBtn);
     newLi.appendChild(editBtn);
     todoList.appendChild(newLi);
@@ -82,9 +125,11 @@ const editTodo = (id) => {
     task.classList.add('hidden');
     inputField.placeholder = task.innerText;
   } else {
-    console.log(inputField.value);
-    todoArr.splice(id, 1, { task: inputField.value });
-    console.log(todoArr);
+    if (inputField.value === '') {
+      todoArr.splice(id, 1, { task: inputField.placeholder });
+    } else {
+      todoArr.splice(id, 1, { task: inputField.value });
+    }
     createTodo(todoArr);
     task.classList.remove('hidden');
     inputField.classList.add('hidden');
@@ -92,9 +137,7 @@ const editTodo = (id) => {
 };
 
 const doneTask = (id) => {
-  console.log('done');
   let task = document.getElementById(`task-${id}`);
-  console.log(task);
   task.classList.contains('strike')
     ? task.classList.remove('strike')
     : task.classList.add('strike');
